8f61c862be4411e0f399bfe872d54c43f17756c7
Akash Kumar <aakashkr343111851@gmail.com>
2026-07-07 15:16:26 +0530

    bugs fixed

--- routes/blogsRoutes.js
+++ routes/blogsRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const multer = require('multer');
 const Blog = require('../models/blogs');
 
 // Home page
@@ -39,7 +39,55 @@ router.get('/blogs/create', (req, res) => {
     });
 });
 
+router.get('/blogs/:id', (req, res) => {
+    const id = req.params.id.trim();
+
+    Blog.findById(id)
+        .then(result => {
+            res.render('details', {
+                title: 'Blog Details',
+                blog: result
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
+
+
 // Add the rest of your blog routes here...
+const upload = multer({ dest: 'public/uploads/' });
+
+router.post('/blogs', upload.single('image'), (req, res) => {
+
+    const blog = new Blog({
+        title: req.body.title,
+        snippet: req.body.snippet,
+        body: req.body.body,
+        image: req.file ? req.file.filename : null
+    });
+
+    blog.save()
+        .then(result => {
+            res.redirect('/blogs');
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
+
+router.delete('/blogs/:id', (req, res) => {
+    Blog.findByIdAndDelete(req.params.id)
+        .then(result => {
+            res.json({
+                redirect: '/blogs'
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: err });
+        });
+});
 
 module.exports = router;
 
